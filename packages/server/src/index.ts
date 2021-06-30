e1e617f8903a6612aeeea42fc0ffff931a09a416
Daniel Hausner <dn.hausner@gmail.com>
2021-06-30 15:56:50 +0200

    Set up CORS and parse application/json and application/x-www-form-urlencoded requests

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,13 +1,31 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+
+const corsOptions = {
+  origin: 'http://localhost:8081',
+};
+
+app.use(cors(corsOptions));
+
+// Parse requests of content-type - application/json
+app.use(express.json());
+
+// Parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
 const getYear = (date: Date): string => {
   return `${date.getFullYear()}`;
 };
+
 app.get('/', (req, res) => {
   const year = getYear(new Date());
-  res.send(`Hello World ! ${year}`);
+  res.json({ message: `Hello World ! ${year}` });
 });
-app.listen(3000, () => {
-  console.log('App listening on port 3000!');
+
+// Set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
