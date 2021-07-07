4b6affef1a284aca09648df4c6d9223b71518a6d
Daniel Hausner <dn.hausner@gmail.com>
2021-07-07 22:58:37 +0200

    imp(Routes): rename /numberOfFilesByType to /fileCount/byType

--- packages/server/src/routes/report.routes.ts
+++ packages/server/src/routes/report.routes.ts
@@ -4,10 +4,10 @@ import * as reports from '../controllers/report.controller';
 const router = Router();
 
 // Get number of files grouped by user
-router.get('/fileCount/:userId', reports.getNumberOfFilesGroupByUser);
+router.get('/fileCount/:userId(\\d+)', reports.getNumberOfFilesGroupByUser);
 
 // Get number of files grouped by file type
-router.get('/numberOfFilesByType', reports.getNumberOfFilesGroupByType);
+router.get('/fileCount/byType', reports.getNumberOfFilesGroupByType);
 
 // Get average file size of all files
 router.get('/averageFileSize', reports.getAverageFileSize);
