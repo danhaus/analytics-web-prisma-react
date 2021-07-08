d037e3dacadba06a92f0f02d91efcd84f3a0909f
Daniel Hausner <dn.hausner@gmail.com>
2021-07-08 16:57:30 +0200

    feat(Router): set up router with one page, UsersPage

--- packages/dashboard/src/App.tsx
+++ packages/dashboard/src/App.tsx
@@ -1,38 +1,33 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import * as React from 'react';
+import { ChakraProvider, theme } from '@chakra-ui/react';
+
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import UsersPage from './users/pages/UsersPage';
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
+    <Router>
+      <ColorModeSwitcher/>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route path="/users">
+          <UsersPage />
+        </Route>
+      </Switch>
+    </Router>
   </ChakraProvider>
-)
+);
---
+++ packages/dashboard/src/users/pages/UsersPage.tsx
@@ -0,0 +1,12 @@
+import { Box, Text } from '@chakra-ui/react';
+import React from 'react';
+
+const UsersPage = () => {
+  return (
+    <Box>
+      <Text>Users Page</Text>
+    </Box>
+  );
+};
+
+export default UsersPage;
