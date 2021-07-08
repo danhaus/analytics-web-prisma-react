8d73e10164891b997bfe89ea1c97b2f49a9e89aa
Daniel Hausner <dn.hausner@gmail.com>
2021-07-08 17:58:24 +0200

    imp(Navigation): remove placeholder navigation components

--- packages/dashboard/src/App.tsx
+++ packages/dashboard/src/App.tsx
@@ -3,32 +3,18 @@ import { ChakraProvider } from '@chakra-ui/react';
 
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import UsersPage from './users/pages/UsersPage';
-import Sidebar from "./navigation/components/Sidebar";
-import theme from "./theme";
+import Sidebar from './navigation/components/Sidebar';
+import theme from './theme';
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Router>
       <Sidebar>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/users">Users</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <Switch>
-        <Route path="/users">
-          <UsersPage />
-        </Route>
-      </Switch>
+        <Switch>
+          <Route path="/users">
+            <UsersPage />
+          </Route>
+        </Switch>
       </Sidebar>
     </Router>
   </ChakraProvider>
